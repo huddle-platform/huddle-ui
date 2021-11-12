3080bed80e5dc7a7a3c7fce3aa2b424ce88eb995
Ole Petersen <peteole2707@gmail.com>
2021-11-12 22:22:51 +0100

    Added example gql query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { useGetMeQuery } from './schemas';
 function App() {
-  //const res=useGetMeQuery();
+  const {data,loading}=useGetMeQuery();
+  if(loading) return <div>Loading...</div>
   return (
     <div className="App">
       <h1>Huddle</h1>
-      This is our great app called Huddle.
+      This is our great app called Huddle. Here we have an example gql query result:
+      {data?.me?.name}
     </div>
   );
 }
