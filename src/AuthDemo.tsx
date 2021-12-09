16bc4c38478fcc85df34f5ab921b4f1950035c86
Ole Petersen <peteole2707@gmail.com>
2021-12-09 23:29:08 +0100

    Added reading token

--- src/AuthDemo.tsx
+++ src/AuthDemo.tsx
@@ -6,19 +6,22 @@ export const AuthDemo: React.FC = () => {
             <h1>Auth Demo</h1>
             <button onClick={async () => {
                 console.log('login');
-                const config = new Configuration({ basePath: 'http://127.0.0.1:4433' });
+                const config = new Configuration({ basePath: 'http://127.0.0.1:8090' });
                 const api = new V0alpha2Api(config);
                 const resp = await api.initializeSelfServiceRegistrationFlowForBrowsers()
                 console.log(resp.data);
+                if (!resp) return
                 const registrationFlowId = resp.data.id;
+                const csrf_token = resp.data.ui.nodes[0].attributes.value as string;
 
                 const registrationResponse = await api.submitSelfServiceRegistrationFlow(registrationFlowId, {
                     method: "password",
                     password: "1234",
                     traits: {
                         email: "ole.petersen@go4more.de"
-                    }
-                },{"Accept": "application/json"})
+                    },
+                    csrf_token: csrf_token
+                })
                 console.log(registrationResponse.data);
             }}>Login</button>
         </div>
