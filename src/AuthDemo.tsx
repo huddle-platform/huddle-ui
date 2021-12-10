4715d6539ce3272f5d7ef16c3669c305d35b8bb4
Ole Petersen <peteole2707@gmail.com>
2021-12-10 08:55:45 +0100

    Updated withCredentials option

--- src/AuthDemo.tsx
+++ src/AuthDemo.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Configuration, V0alpha2Api } from '@ory/kratos-client'
+const config = new Configuration({
+    basePath: 'http://localhost:8090',
+    baseOptions: {
+        withCredentials: true
+    }
+});
+const api = new V0alpha2Api(config);
 export const AuthDemo: React.FC = () => {
     return (
         <div>
             <h1>Auth Demo</h1>
             <button onClick={async () => {
-                console.log('login');
-                const config = new Configuration({ basePath: 'http://127.0.0.1:8090' });
-                const api = new V0alpha2Api(config);
+                console.log('registering user');
                 const resp = await api.initializeSelfServiceRegistrationFlowForBrowsers()
                 console.log(resp.data);
                 if (!resp) return
@@ -16,14 +21,31 @@ export const AuthDemo: React.FC = () => {
 
                 const registrationResponse = await api.submitSelfServiceRegistrationFlow(registrationFlowId, {
                     method: "password",
-                    password: "1234",
+                    password: "pi<uewfpiwduciQD",
                     traits: {
                         email: "ole.petersen@go4more.de"
                     },
                     csrf_token: csrf_token
                 })
                 console.log(registrationResponse.data);
-            }}>Login</button>
+            }}>register</button>
+
+            <button onClick={async () => {
+                console.log('login');
+                const resp = await api.initializeSelfServiceLoginFlowForBrowsers()
+                console.log(resp.data);
+                if (!resp) return
+                const loginFlowId = resp.data.id;
+                const csrf_token = resp.data.ui.nodes[0].attributes.value as string;
+
+                const registrationResponse = await api.submitSelfServiceLoginFlow(loginFlowId, undefined, {
+                    method: "password",
+                    password: "pi<uewfpiwduciQD",
+                    password_identifier: "ole.petersen@go4more.de",
+                    csrf_token: csrf_token
+                })
+                console.log(registrationResponse.data);
+            }}>login</button>
         </div>
     )
 }
