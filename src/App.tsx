18571be8092ffd3f009b2e8a003e38a63abf600c
Peter Ridilla <peter.ridilla@tum.de>
2021-12-21 18:42:11 +0100

    HomePage created

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,13 @@ import { useGetChatsQuery } from './schemas';
 import ShowList from './project-list/project-list';
 import ShowDetail from './project-detail/project-detail';
 import Home from './home-header/home-header';
+import HomePage from './home-page/home-page';
 
 function App() {
   const { data, loading } = useGetChatsQuery();
   if (loading) return <div>Loading...</div>
   return (
-    <div className="App">
-      
-      {/* {data?.messages.map(message => (<p>ID:{message.id}, Message:{message.message}</p>))} */}
-        <Home></Home>
-        <ShowList></ShowList>
-        <ShowDetail></ShowDetail>
-    </div>
+    <HomePage></HomePage>
   );
 }
 
