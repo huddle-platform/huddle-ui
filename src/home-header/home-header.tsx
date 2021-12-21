28f77cb3b12d05d22e3e26844d992a462e032df4
Peter Ridilla <peter.ridilla@tum.de>
2021-12-21 19:52:46 +0100

    Header design

--- src/home-header/home-header.tsx
+++ src/home-header/home-header.tsx
@@ -1,7 +1,29 @@
 import './home-header.css'
 import React, { useRef, useLayoutEffect, useState, useEffect  } from "react";
-import { Button } from '@mui/material';
 import FilterListIcon from '@mui/icons-material/FilterList';
+
+const CategoryButton = ({category = "", activated = false}) => {
+    return (
+        <div className={activated ? "home-header-category-button-activated" : "home-header-category-button"}>
+            {category}
+        </div>
+    );
+}
+
+const CategoryList = () => {
+    return (
+        <div className="home-header-category-list">
+            <CategoryButton category="All"></CategoryButton>
+            <CategoryButton category="Social" activated={true}></CategoryButton>
+            <CategoryButton category="Tinker"></CategoryButton>
+            <CategoryButton category="Code"></CategoryButton>
+            <CategoryButton category="Design"></CategoryButton>
+            <CategoryButton category="Sports"></CategoryButton>
+            <CategoryButton category="Outdoors"></CategoryButton>
+            <CategoryButton category="More"></CategoryButton>
+        </div> 
+    );
+}
 
 const HomeHeader = () => {
     const targetRef = useRef();
@@ -23,18 +45,21 @@ const HomeHeader = () => {
         };
     }, []);
 
-    const opac = Math.max(1.0 - scrollPosition/100.0, 0.0)
+    const opac = Math.max(1.0 - scrollPosition/200.0, 0.0)
+    const rotLogo = scrollPosition;
 
     return (
         <div className="home-header">
-            <h1 className="home-header-title">Huddle</h1>
+            <img src={"/huddle-logo.png"} className="home-logo-huddle-icon"
+                style={{transform: `rotate(${rotLogo}deg)`, opacity: opac}}></img>
+
             <h1 className="home-header1" style={{opacity: opac}}>Find your next</h1>
-            <h1 className="home-header2" style={{opacity: opac}}>student project {scrollPosition}</h1>
-            <p></p>
+            <h1 className="home-header2" style={{opacity: opac}}>student project</h1>
             <button className="home-filter-button">
-                <FilterListIcon className="home-filter-icon"></FilterListIcon>
+                <FilterListIcon className="home-filter-icon" ></FilterListIcon>
                 <span className="home-filter-button-text">Filter</span>
             </button>
+            <CategoryList></CategoryList>
         </div>
     );
 }
